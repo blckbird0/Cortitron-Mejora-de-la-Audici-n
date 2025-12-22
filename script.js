8848b80beb4f8ed9ee00854264eec2498fb26f6a
blckbird0 <tibielmoreira@gmail.com>
2025-12-22 16:13:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Affiliate link
+const AFFILIATE_URL = "https://uh8f8d208auh.uewhbgfvds.cc/?rid=-7EBNQCgQAAHD1WwYABgEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx";
+
+function goToAffiliate() {
+  window.open(AFFILIATE_URL, "_blank");
+}
+
+// Simple countdown placeholder
+let time = 13*3600 + 59*60 + 59; 
+const timerEl = document.getElementById("timer");
+
+function updateTimer() {
+  const h = Math.floor(time / 3600);
+  const m = Math.floor((time % 3600) / 60);
+  const s = time % 60;
+  timerEl.textContent = `${h}:${m}:${s}`;
+  if (time > 0) time--;
+}
+setInterval(updateTimer, 1000);
